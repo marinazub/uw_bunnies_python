4d6d956aeda87af20f0e23822571b9f172dccadc
Marina Zub <mazub91@gmail.com>
2024-04-30 11:05:35 -0700

    Update project.py

--- project.py
+++ project.py
@@ -4,11 +4,14 @@ Input: product and quantity
 Output: order details
 """
 
+import datetime
+import random
+
 #global
 order_ID = 1000
 products = {
-    101: {'name':'book', 'price': 15.0},
-    102: {'name':'notepad', 'price': 10.0},
+    101: {'name':'headphones', 'price': 15.0},
+    102: {'name':'laptop', 'price': 10.0},
     103: {'name':'pen', 'price': 3.0},
 }
 
@@ -22,30 +25,34 @@ def compute_price(name, count):
         return tot_price, pid
     
 def get_date():
-    start = 2020
-    end = 2023
+    start = 0
+    end = 365 * 4 + 1
     numbers = []
     x = start
     while x <= end:
         numbers.append(x)
         x = x + 1
 
-    choice = random.choice(numbers)
-    return choice
+    days = random.choice(numbers)
+
+    start_date = datetime.date(2020,1,1)
+    order_d = start_date + datetime.timedelta(days)
+
+    return order_d
 
 def submit():
     global order_ID
     #inputs
     product = input('Enter the name of product headphone, laptop or pen >')
-    x = [sku['name'] for sku in items.values()]
+    x = [sku['name'] for sku in products.values()]
     count = 0
 
     if product in x:
         count = int(input('Enter the quantity of the product >'))
-        #order_price = compute_order_price(product, count)
-        #date = get_date()
+        order_price = compute_price(product, count)
+        date = get_date()
         order_ID += 1
-        #order_date_string = date.strftime("%d-%m-%y")
+        order_date_string = date.strftime("%d-%m-%y")
     else: 
         print ('Invalid. Try again.')
         submit()
@@ -53,20 +60,26 @@ def submit():
     print(f'''
         product: {product} 
         count: {count} 
-        total price: {product_price_total[0]}
-        product_id: {product_price_total[1]}
+        total price: {order_price[0]}
+        product_id: {order_price[1]}
         order_date: {order_date}
-        order id: {num_input}
+        order id: {order_ID}
             ''')
+
+def reset():
+    global order_ID
+    order_ID = 10000 
     
 #main
 quit = False
 while not quit:
-    print('1.Submit 2.Exit')
+    print('1.Submit 2.Reset 3.Exit')
     choice = int(input('Enter 1, or 2> '))
     if choice == 1:
         submit()
     elif choice == 2:
+        reset()
+    elif choice == 3:
         quit = True
     else:
         print('Invalid choice!')
